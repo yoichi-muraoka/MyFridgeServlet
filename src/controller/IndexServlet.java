d16ca171d309e6c25a82d907b06d7c18284955d5
zdis56 <zdis56@sim.zdrv.com>
2021-09-29 13:16:22 +0900

    アイテムリストの取得
    items.txtは、eclipse直下に配置

--- src/controller/IndexServlet.java
+++ src/controller/IndexServlet.java
@@ -1,12 +1,17 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dto.Item;
+import service.ItemService;
+import service.ItemServiceImpl;
 
 /**
  * Servlet implementation class IndexServlet
@@ -15,6 +20,15 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// アイテムリストの取得
+		ItemService service = new ItemServiceImpl();
+		List<Item> list = service.getItemList();
+
+		for(Item item : list) {
+			System.out.print(item.getExpDate());
+			System.out.println(item.getName());
+		}
+
 		request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
 	}
 
