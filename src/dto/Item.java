4d1be0f8e32b15b32805818b8368efc943b8ae66
zdis56 <zdis56@sim.zdrv.com>
2021-09-30 09:37:34 +0900

    Itemクラスに削除用IDを保持するためのフィールドを追加

--- src/dto/Item.java
+++ src/dto/Item.java
@@ -8,6 +8,7 @@ import java.util.Date;
  */
 public class Item {
 
+	private int id;       //削除時に使用するID番号
 	private Date expDate; //賞味期限(Expiration Date)
 	private String name;  //アイテム名
 
@@ -17,10 +18,25 @@ public class Item {
 		this.name = name;
 	}
 
+	public Item(int id, Date expDate, String name) {
+		this.id = id;
+		this.expDate = expDate;
+		this.name = name;
+	}
+
 	// アクセッサ
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public Date getExpDate() {
 		return expDate;
 	}
+
 
 	public void setExpDate(Date expDate) {
 		this.expDate = expDate;
